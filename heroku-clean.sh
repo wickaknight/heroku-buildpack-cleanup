ce4a7558a8353caa5fb86685d70cc91167897215
madhushankara <madhushankqra@gmail.com>
2025-01-12 09:14:50 +0000

    Fix Heroku boot timeout by binding to  and optimizing runtime setup

--- heroku-clean.sh
+++ heroku-clean.sh
@@ -4,17 +4,15 @@ echo "Starting post-build cleanup..."
 
 # Clear unnecessary libraries
 echo "Clearing Python packages not used in the project..."
-pip freeze | grep -vE "Flask==3.1.0|Pillow==11.1.0|Requests==2.32.3|torch==2.1.0\+cpu|torchaudio==2.1.0\+cpu|transformers==4.47.1|TTS==0.22.0|gunicorn" | xargs pip uninstall -y
+pip uninstall -y Cython Jinja2 Markdown MarkupSafe PyYAML Pygments SudachiDict-core SudachiPy \
+Unidecode Werkzeug matplotlib scikit-learn umap-learn babel langcodes dateparser
 
 # Remove caches and temporary files
 echo "Removing temporary files and caches..."
 rm -rf ~/.cache/pip
-rm -rf ~/.cache
 rm -rf venv/
 find . -name "*.pyc" -exec rm -f {} \;
 find . -name "*.pyo" -exec rm -f {} \;
 
 # Remove other unneeded directories (if present)
 rm -rf tests/ docs/ examples/
-
-echo "Cleanup completed."
